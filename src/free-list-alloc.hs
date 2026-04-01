0709ec1e6cf54a1c527ad2360de82f7d69556c85
Nikolay Alferuk <alferukn@gmail.com>
2026-04-01 17:51:59 +0300

    feat: add fun deallocate

--- src/free-list-alloc.hs
+++ src/free-list-alloc.hs
@@ -8,21 +8,67 @@ data Block = Block {
         bSize :: Size
 } deriving (Show, Eq)
 
-data Allocator = Allocator {
+data AllocatorState = AllocatorState {
         freeBlocks :: [Block]
-}
+} deriving (Show, Eq)
 
-initAllocator :: Size -> Allocator
-initAllocator maxSize = Allocator {
+initAllocator :: Size -> AllocatorState
+initAllocator maxSize = AllocatorState {
         freeBlocks = [Block 0 maxSize]
 }
 
-allocate :: Size -> Allocator -> (Maybe Address, Allocator)
-allocate needSize (Allocator blocks) = 
+allocate :: Size -> AllocatorState -> (Maybe Address, AllocatorState)
+allocate needSize (AllocatorState blocks) =
         let (before, found) = break (\b -> bSize b >= needSize) blocks in
         case found of
-                [] -> (Nothing, Allocator blocks)
-                (targetBlock : after) -> (Just addr, Allocator newFreeBlocks) where
-                        addr = bAddr targetBlock 
+                [] -> (Nothing, AllocatorState blocks)
+                (targetBlock : after) -> (Just addr, AllocatorState newFreeBlocks) where
+                        addr = bAddr targetBlock
                         newFreeBlock = Block (addr + needSize) (bSize targetBlock - needSize)
                         newFreeBlocks = before ++ (if bSize newFreeBlock > 0 then [newFreeBlock] else []) ++ after
+
+deallocate :: Block -> AllocatorState -> AllocatorState
+deallocate (Block deAddr deSize) (AllocatorState freeBlocks) =          -- will be rewrite
+        let endAddr = deAddr + deSize
+            (before, found) = break (\ b -> endAddr <= bAddr b) freeBlocks  --O(n)
+        in case found of
+
+                [] ->   if null before
+                        then AllocatorState [Block deAddr deSize]
+                        else let
+                                prevBlock = last before             --O(n)
+                                blocksBeforePrev = init before      --O(n)
+                                prevAddr = bAddr prevBlock
+                                prevSize = bSize prevBlock
+                             in if (prevAddr + prevSize) == deAddr
+                                --(++) - O(n). Quadruple O(n) in branch
+                                then AllocatorState (blocksBeforePrev ++ [Block prevAddr (prevSize + deSize)])
+                                else AllocatorState (before ++ [Block deAddr deSize])
+
+                (nextBlock : after) -> let
+                        nextAddr = bAddr nextBlock
+                        nextSize = bSize nextBlock
+                        in if endAddr == nextAddr
+                        then if null before
+                                then AllocatorState (Block deAddr (deSize + nextSize) : after)
+                                else let
+                                        prevBlock = last before
+                                        blocksBeforePrev = init before
+                                        prevAddr = bAddr prevBlock
+                                        prevSize = bSize prevBlock
+                                     in 
+                                        if (prevAddr + prevSize) == deAddr
+                                        then AllocatorState (blocksBeforePrev ++ [Block prevAddr (prevSize + deSize + nextSize)] ++ after)
+                                        else AllocatorState (before ++ [Block deAddr (deSize + nextSize)] ++ after)
+
+                        else if null before
+                                then AllocatorState ([Block deAddr deSize] ++ found)
+                                else let 
+                                        prevBlock = last before
+                                        blocksBeforePrev = init before
+                                        prevAddr = bAddr prevBlock
+                                        prevSize = bSize prevBlock
+                                     in 
+                                        if (prevAddr + prevSize) == deAddr
+                                        then AllocatorState (blocksBeforePrev ++ [Block prevAddr (prevSize + deSize)] ++ found)
+                                        else AllocatorState (before ++ [Block deAddr deSize] ++ found)
